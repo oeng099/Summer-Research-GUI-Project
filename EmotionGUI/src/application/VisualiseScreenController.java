e7cca6bf4f5014d9edcba8a46a7680485c976e6c
oeng099 <oeng099@aucklanduni.ac.nz>
2021-12-10 17:13:21 +1300

    Added functionality for Main Menu button in VisualiseScreen scene

--- EmotionGUI/src/application/VisualiseScreenController.java
+++ EmotionGUI/src/application/VisualiseScreenController.java
@@ -4,6 +4,8 @@ import java.io.File;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -40,5 +42,16 @@ public class VisualiseScreenController {
 			fileName.setText(file.toString());
 		}
 	}
-
+	
+	public void returnToMainMenu(ActionEvent event) {
+		try {
+			root = FXMLLoader.load(getClass().getResource("fxml/HomeScreen.fxml"));
+			stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+			scene = new Scene(root,borderPane.getWidth(),borderPane.getHeight());
+			stage.setScene(scene);
+			stage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
