17a18f9ae492908272763ac1f98156bdbe399a55
oeng099 <oeng099@aucklanduni.ac.nz>
2022-02-16 00:42:54 +1300

    Created script to run EmotionGUI from command line

---
+++ Run_EmotionGUI.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#Run the program
+java -Djdk.gtk.version=2 --module-path /home/student/javafx-sdk-11.0.2/javafx --add-modules javafx.controls,javafx.media,javafx.base,javafx.fxml -jar EmotionGUI.jar
