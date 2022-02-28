f66c515fc5312b091e5c549c27c4b210a98c00cd
Owen Eng <oeng099@aucklanduni.ac.nz>
2022-02-28 16:31:35 +1300

    Commented HoverNode class

--- EmotionGUI/src/application/HoverNode.java
+++ EmotionGUI/src/application/HoverNode.java
@@ -8,10 +8,14 @@ import javafx.scene.layout.StackPane;
 
 public class HoverNode extends StackPane {
 
+	// Constructor of the HoverNode class, takes inputs of valence and arousal
+	// coordinates
 	public HoverNode(String valenceCoordinate, String arousalCoordinate) {
-		
-		Label label = createDataLabel(valenceCoordinate,arousalCoordinate);
-		
+
+		// Creates the label that will appear when hovered over
+		Label label = createDataLabel(valenceCoordinate, arousalCoordinate);
+
+		// When the mouse enters a point it will display the label
 		setOnMouseEntered(new EventHandler<MouseEvent>() {
 			@Override
 			public void handle(MouseEvent mouseEvent) {
@@ -21,6 +25,7 @@ public class HoverNode extends StackPane {
 			}
 		});
 
+		// When the mouse exits a point, its label will disappear
 		setOnMouseExited(new EventHandler<MouseEvent>() {
 			@Override
 			public void handle(MouseEvent mouseEvent) {
@@ -29,16 +34,17 @@ public class HoverNode extends StackPane {
 			}
 		});
 	}
-	
 
-public Label createDataLabel(String valenceCoordinate, String arousalCoordinate) {
-	Label dataLabel = new Label(valenceCoordinate + "," + arousalCoordinate);
-	dataLabel.getStyleClass().addAll("default-color0", "chart-line-symbol", "chart-series-line");
-    dataLabel.setStyle("-fx-font-size: 18; -fx-font-weight: bold; -fx-font-family: Montserrat SemiBold");
-    
-    dataLabel.setMinSize(Label.USE_PREF_SIZE, Label.USE_PREF_SIZE);
-    
-	return dataLabel;
-}
+	//Method to create a label 
+	public Label createDataLabel(String valenceCoordinate, String arousalCoordinate) {
+		//Creates the label with valence and arousal coordinates
+		Label dataLabel = new Label(valenceCoordinate + "," + arousalCoordinate);
+		//Sets the style for the label
+		dataLabel.getStyleClass().addAll("default-color0", "chart-line-symbol", "chart-series-line");
+		dataLabel.setStyle("-fx-font-size: 18; -fx-font-weight: bold; -fx-font-family: Montserrat SemiBold");
+		dataLabel.setMinSize(Label.USE_PREF_SIZE, Label.USE_PREF_SIZE);
+
+		return dataLabel;
+	}
 
 }
